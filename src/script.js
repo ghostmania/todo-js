abe15172ee0a94201e688d2fd3328ddb40c6bf88
ghostmania <ghostmania2@gmail.com>
2017-03-07 16:07:53 +0300

    added input for editing <li> values

--- src/script.js
+++ src/script.js
@@ -16,14 +16,21 @@ function add_element() { // create element function
             checkbox.setAttribute("type", "checkbox"); // make input look like checkbox
             checkbox.setAttribute("onclick", "checkUncheck(this)")
             li.appendChild(checkbox); // insert checkbox into <li>
+            var span = document.createElement("span"); // create span
+            span.setAttribute("ondblclick", "edit(this)") // attach edit function to created span
+            li.appendChild(span); // insert checkbox into <li>
             var textnode = document.createTextNode(textInput); // create text node "textInput"
-            li.appendChild(textnode); // insert text into <li>
-            var remove = document.createElement("button"); // create <span> element
+            span.appendChild(textnode); // insert text into <span>
+            var edit_input = document.createElement('input'); // create input for edit
+            edit_input.setAttribute("style", "display: none"); // hide edit input
+            edit_input.setAttribute("class", "edit"); // attach class to edit input
+            li.appendChild(edit_input); // insert checkbox into <li>
+            var remove = document.createElement("button"); // create <button> element
             var removeNode = document.createTextNode("x"); // create text node "x"
             remove.setAttribute("class", "removeBtn"); // add id to remove button
             remove.setAttribute("onclick", "remove_element(this)"); // set onclick function to remove button
-            remove.appendChild(removeNode); // insert "x" into <span>
-            li.appendChild(remove); // insert <span> into <li>
+            remove.appendChild(removeNode); // insert "x" into <button>
+            li.appendChild(remove); // insert <button> into <li>
             document.getElementById("list").appendChild(li); // insert <li> into <ul>
             li.setAttribute("class", "active");
             document.getElementById("inputField").value = ''; // clear input field
@@ -72,3 +79,15 @@ function show(status) {
         }
     }
 }
+
+function edit(e) {
+    var children = e.parentNode.children; // find li for edit
+    for (var i = 0; i < children.length; i++){
+        if (children[i].className == 'edit') {
+            children[i].setAttribute("style", "display: block"); // show edit input
+            children[i].value = e.textContent; // insert <span> value to input
+            e.setAttribute("style", "display: none");  // hide span
+        }
+    }
+ }
+
