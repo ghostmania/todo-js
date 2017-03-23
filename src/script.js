69b5c50a95ad1f10220ddd9065e03071ef71e3c3
evgeny kirilenko <jinkaz@bk.ru>
2017-03-23 15:43:15 +0300

    code refactoring

--- src/script.js
+++ src/script.js
@@ -5,11 +5,11 @@ var completed = document.getElementById('completed');
 var page_qty = 1;
 var currentPage = 1;
 var actualList = [];
-
+var list = document.getElementById('list');
+var buttons = document.getElementById('buttons');
 document.getElementById("inputField").focus(); // focus input after adding element
 
 window.onload = function () {
-    var list = document.getElementById('list');
     if (list.children.length > 0){ // if list isn't empty
        page_qty = Math.ceil(list.children.length / 3); // count pages
     }
@@ -18,67 +18,63 @@ window.onload = function () {
 
 function iterateCreatePages(){ // create pages according to elements count
     for (var i = 0; i < page_qty; i++){
-        var btns = document.getElementById('buttons'); // create buttons
         var new_btn = document.createElement('button');
-        btns.appendChild(new_btn);
+        buttons.appendChild(new_btn);
         new_btn.innerHTML = (i + 1).toString() ; // give values to buttons
         new_btn.setAttribute('onclick', 'showPage('+(i+1)+')'); // show elements on first page
     }
 }
 
-
-function showPage(e) {
-    currentPage = e;
+function showPage(pageQty) {
+    currentPage = pageQty;
     showItemsPerPage(currentTab);
 }
 
-function add_element_on_enter(e) { // add element on Enter
-    if (e.keyCode == 13) {
+function add_element_on_enter(pressedBtn) { // add element on Enter
+    if (pressedBtn.keyCode == 13) {
         add_element();
     }
 }
 
 function add_element() { // create element function
     var textInput = document.getElementById("inputField").value; // get input value
-        if (textInput.trim() !== '') {
-            var li = document.createElement("LI"); // create <li> element
-            var checkbox = document.createElement("input"); // create <input> element
-            checkbox.setAttribute("type", "checkbox"); // make input look like checkbox
-            checkbox.setAttribute("onclick", "checkUncheck(this)");
-            li.appendChild(checkbox); // insert checkbox into <li>
-            var span = document.createElement("span"); // create span
-            span.setAttribute("ondblclick", "edit(this)"); // attach edit function to created span
-            span.setAttribute("class", "active");
-            li.appendChild(span); // insert checkbox into <li>
-            var textnode = document.createTextNode(textInput); // create text node "textInput"
-            span.appendChild(textnode); // insert text into <span>
-            var edit_input = document.createElement('input'); // create input for edit
-            edit_input.setAttribute("style", "display: none"); // hide edit input
-            edit_input.setAttribute("class", "edit"); // attach class to edit input
-            edit_input.setAttribute("onkeypress", "edited_value(event)");
-            li.appendChild(edit_input); // insert checkbox into <li>
-            var remove = document.createElement("button"); // create <button> element
-            var removeNode = document.createTextNode("x"); // create text node "x"
-            remove.setAttribute("class", "removeBtn"); // add id to remove button
-            remove.setAttribute("onclick", "remove_element(this)"); // set onclick function to remove button
-            remove.appendChild(removeNode); // insert "x" into <button>
-            li.appendChild(remove); // insert <button> into <li>
-            document.getElementById("list").appendChild(li); // insert <li> into <ul>
-            document.getElementById("inputField").value = ''; // clear input field
-            currentTab == 'completed' && li.setAttribute("style", "display: none"); // don't display new element on completed tab
-            document.getElementById("inputField").focus(); // focus input after adding element
-            all.textContent = Number(all.textContent) + 1; // change number of all elements
-            active.textContent = Number(active.textContent) + 1; // change number of active elements
-            showItemsPerPage(currentTab);
-
-            page_qty = Math.ceil(actualList.length / 3); // count pages
-            if (page_qty > document.getElementById('buttons').children.length){ // if there are more then  3 elements in list, create new page
-                var new_btn = document.createElement('button');
-                new_btn.innerHTML = page_qty.toString();
-                document.getElementById('buttons').appendChild(new_btn);
-                new_btn.setAttribute('onclick', 'showPage('+page_qty+')'); // show elements according to current page
-            }
+    if (textInput.trim() !== '') {
+        var li = document.createElement("LI"); // create <li> element
+        var checkbox = document.createElement("input"); // create <input> element
+        checkbox.setAttribute("type", "checkbox"); // make input look like checkbox
+        checkbox.setAttribute("onclick", "checkUncheck(this)");
+        li.appendChild(checkbox); // insert checkbox into <li>
+        var span = document.createElement("span"); // create span
+        span.setAttribute("ondblclick", "edit(this)"); // attach edit function to created span
+        span.setAttribute("class", "active");
+        li.appendChild(span); // insert checkbox into <li>
+        var textnode = document.createTextNode(textInput); // create text node "textInput"
+        span.appendChild(textnode); // insert text into <span>
+        var edit_input = document.createElement('input'); // create input for edit
+        edit_input.setAttribute("style", "display: none"); // hide edit input
+        edit_input.setAttribute("class", "edit"); // attach class to edit input
+        edit_input.setAttribute("onkeypress", "edited_value(event)");
+        li.appendChild(edit_input); // insert checkbox into <li>
+        var remove = document.createElement("button"); // create <button> element
+        remove.setAttribute("class", "removeBtn"); // add id to remove button
+        remove.setAttribute("onclick", "remove_element(this)"); // set onclick function to remove button
+        remove.textContent = "x";
+        li.appendChild(remove); // insert <button> into <li>
+        list.appendChild(li); // insert <li> into <ul>
+        document.getElementById("inputField").value = ''; // clear input field
+        currentTab == 'completed' && li.setAttribute("style", "display: none"); // don't display new element on completed tab
+        document.getElementById("inputField").focus(); // focus input after adding element
+        all.textContent = Number(all.textContent) + 1; // change number of all elements
+        active.textContent = Number(active.textContent) + 1; // change number of active elements
+        showItemsPerPage(currentTab);
+        page_qty = Math.ceil(actualList.length / 3); // count pages
+        if (page_qty > buttons.children.length){ // if there are more then  3 elements in list, create new page
+            var new_btn = document.createElement('button');
+            new_btn.innerHTML = page_qty.toString();
+            buttons.appendChild(new_btn);
+            new_btn.setAttribute('onclick', 'showPage('+page_qty+')'); // show elements according to current page
         }
+    }
 }
 
 function nextPage() { // change page when press ">"
@@ -89,19 +85,18 @@ function nextPage() { // change page when press ">"
 }
 
 function prevPage() { // change page when press "<"
-        if (currentPage != 1) {
-            currentPage = currentPage - 1;
-        }
+    if (currentPage != 1) {
+        currentPage = currentPage - 1;
         showItemsPerPage(currentTab);
+    }
 }
 
-function showItemsPerPage(e) {
-    var list = document.getElementById('list');
+function showItemsPerPage(tab) {
     actualList = [];
-    if (e != 'all') {
+    if (tab != 'all') {
         for (var i = 0; i < list.children.length; i++) {
             list.children[i].setAttribute('style', 'display: none');
-            if (list.children[i].getElementsByTagName('span')[0].className == e) {
+            if (list.children[i].getElementsByTagName('span')[0].className == tab) {
                 actualList.push(list.children[i]);
              }
         }
@@ -110,17 +105,13 @@ function showItemsPerPage(e) {
         actualList = list.children;
     }
     for (var a = 0; a < actualList.length; a++){
-        if ((currentPage * 3 - 3) <= a && a <= (currentPage * 3 - 1)) { // if true we show on page
-            actualList[a].setAttribute('style', 'display: block');
-        }
-        else {
+        (currentPage * 3 - 3) <= a && a <= (currentPage * 3 - 1) ?
+            actualList[a].setAttribute('style', 'display: block') :
             actualList[a].setAttribute('style', 'display: none');
-        }
     }
 }
 
 function remove_element(currentLi) { // remove element function
-    var list = document.getElementById("list"); // select list
     var liToDelete = currentLi.parentNode; // select  <li>
     list.removeChild(liToDelete); // remove <li> from <list>
     all.textContent = Number(all.textContent) - 1; // change number of all elements on delete
@@ -137,17 +128,15 @@ function remove_element(currentLi) { // remove element function
     if (list.children.length > 0) { // recount page qty to prevent 0 pages
         page_qty = Math.ceil(list.children.length / 3); // count pages
     }
-    if (page_qty < document.getElementById('buttons').children.length){ // if there are more then  3 elements in list, create new page
-        var lbtn = document.getElementById('buttons').children[document.getElementById('buttons').children.length - 1]; // find last btn
-        document.getElementById('buttons').removeChild(lbtn); // remove last btn
+    if (page_qty < buttons.children.length){ // if there are more then  3 elements in list, create new page
+        var lbtn = buttons.children[buttons.children.length - 1]; // find last btn
+        buttons.removeChild(lbtn); // remove last btn
     }
     recountPages(); // recount pages 
     if(currentPage > buttons.children.length){ // if there is no such page
        currentPage = buttons.children.length; // change page to last page
     }
     showItemsPerPage(currentTab); // show items for current page
-
-
 }
 
 function checkUncheck(currentLi) { //click on checkbox
@@ -179,7 +168,6 @@ function checkUncheck(currentLi) { //click on checkbox
             }
         }
     }
-
     function checked(){ // change number of completed tasks when click on checkbox
         completed.textContent = Number(completed.textContent) + 1; // change number of completed elements on delete
         active.textContent = Number(active.textContent) - 1; // change number of active elements on delete
@@ -197,9 +185,8 @@ function checkUncheck(currentLi) { //click on checkbox
 }
 
 function recountPages() {
-    var btns = document.getElementById('buttons');
-    while (btns.firstChild) {
-        btns.removeChild(btns.firstChild);
+    while (buttons.firstChild) {
+        buttons.removeChild(buttons.firstChild);
     }
     actualList.length == 0 ? page_qty = 1 : page_qty = Math.ceil(actualList.length / 3);
     iterateCreatePages();
@@ -207,45 +194,46 @@ function recountPages() {
 }
 
 function show(status) { // display statuses
-    var ulist = document.getElementById("list");
-    var allLi = ulist.getElementsByTagName('li');
     currentTab = status;
     currentPage = 1;
     showItemsPerPage(currentTab);
     recountPages();
     for(i = 0; i < document.getElementsByClassName('statusTabs').length; i++) { // change butons color to defaults
-        document.getElementsByClassName('statusTabs')[i].setAttribute('style', 'background-color: lemonchiffon');
+        document.getElementsByClassName('statusTabs')[i].setAttribute('style', 'background-color: whitesmoke');
         if (status == 'all') { // change button color when click All button
-        document.getElementsByClassName('statusTabs')[0].setAttribute('style', 'background-color: cyan');
+        document.getElementsByClassName('statusTabs')[0].setAttribute('style', 'background-color: chartreuse');
         } else if (status == 'active') { // change button color when click active button
-            document.getElementsByClassName('statusTabs')[1].setAttribute('style', 'background-color: cyan');
+            document.getElementsByClassName('statusTabs')[1].setAttribute('style', 'background-color: chartreuse');
         } else { // change button color when click completed button
-            document.getElementsByClassName('statusTabs')[2].setAttribute('style', 'background-color: cyan');
+            document.getElementsByClassName('statusTabs')[2].setAttribute('style', 'background-color: chartreuse');
         }
     }
 }
 
-
-function edit(e) { // click on input to change it's value
-    var children = e.parentNode.children; // find li for edit
+function edit(liToEdit) { // click on input to change it's value
+    var children = liToEdit.parentNode.children; // find li for edit
     for (var i = 0; i < children.length; i++){
         if (children[i].className == 'edit') {
+            liToEdit.parentNode.getElementsByTagName("input")[0].setAttribute("style", "display: none"); // hide checkbox
+            liToEdit.parentNode.getElementsByTagName("button")[0].setAttribute("style", "display: none"); // hide remove btn
+            liToEdit.setAttribute("style", "display: none");  // hide span
             children[i].setAttribute("style", "display: inline-block"); // show edit input
-            children[i].value = e.textContent; // insert <span> value to input
+            children[i].value = liToEdit.textContent; // insert <span> value to input
             children[i].focus();
-            e.setAttribute("style", "display: none");  // hide span
         }
     }
  }
 
-function edited_value(e) {
-    if (e.keyCode == 13) {
-        var children = e.target.parentNode.children; // find li for edit
+function edited_value(editedLi) {
+    if (editedLi.keyCode == 13) {
+        var children = editedLi.target.parentNode.children; // find li children for edit
         for (var i = 0; i < children.length; i++){
             if (children[i].tagName == 'SPAN') {
                 children[i].setAttribute("style", "display: inline-block"); // show edited <span>
-                children[i].textContent = e.target.value; // insert <input> value to <span>
-                e.target.setAttribute("style", "display: none");  // hide <input>
+                children[i].textContent = editedLi.target.value; // insert <input> value to <span>
+                editedLi.target.setAttribute("style", "display: none");  // hide <input>
+                editedLi.target.parentNode.getElementsByTagName("input")[0].setAttribute("style", "display: inline-block"); // hide checkbox
+                editedLi.target.parentNode.getElementsByTagName("button")[0].setAttribute("style", "display: inline-block"); // hide remove btn
             }
         }
     }
